9a04328fb37fb0e383849147ed3b49eb378793f3
Jackson <jacksonbull1987@gmail.com>
2020-12-28 16:54:37 -0500

    adding api functions and helper functions to assist with parsing, cleaning

---
+++ helper_funct.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from cm_api import *
+
+#function to parse data list into pandas dataframe
+def create_dataframe(data_list):
+    top_tracks_bucket = []
+    for track in data_list:
+        track_tuple = (track['id'], track['name'], track['isrc'], track['cm_track'],
+                      track['artist'], track['artist_names'], track['album_title'], 
+                       track['album_names'], track['rank'], track['added_at'], track['created_at'],
+                       track['country'],
+                      track['genre'], track['kind'], track['playcount_all'], 
+                       track['playcount_weekly'], 
+                       track['velocity'], track['time_on_chart'])
+        top_tracks_bucket.append(track_tuple)
+
+    top_df = pd.DataFrame(top_tracks_bucket, columns=['SC ID', 'title', 'isrc', 'cm_track_id', 'artist', 'artist_names', 'album_title', 'album_names', 'rank',
+                                            'added_at','created_at', 'country', 'genre', 'kind', 'total_playcount', 'weekly_playcount', 'velocity', 'time_on_chart'])
+    return top_df
+
+#create a list of unique values given an array
+def unique(series):
+    return list(series.unique())
+
+#create a dictionary from a list of tuples
+def Convert(tup, di):
+    new_tup = [x for x in tup if x != None and type(x) == tuple]
+    di = dict(new_tup)
+    return di 
+
+#Given specified parameters, this function returns a dictionary of each artist's fan metric
+def artist_fanmetric_dict(artist_id_dict, api_token, source, since_date, until_date, field_value, dict_value):
+    #dictionary_values: weekly_diff, weekly_diff_percent, monthly_diff, monthly_diff_percent, value
+    bucket = []
+    for k,v in artist_id_dict.items():
+        listener_data = get_fan_metrics(api_token, v, source, since_date,until_date,field=field_value)[field_value]
+        if len(listener_data) > 0:
+            monthly_li_tu = (k.lower(), listener_data[0][dict_value])
+            bucket.append(monthly_li_tu)
+        else:
+            continue
+    dictionary = {}    
+    di = Convert(bucket, dictionary)
+    return di
+    
